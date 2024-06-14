b750878614fdaad0f36ea16481b545b7ef231c77
dcotelessa <david@cotelessa.com>
2024-06-14 16:55:34 -0700

    holy cow a 3-d dot!

--- src/components/react/canvas.jsx
+++ src/components/react/canvas.jsx
@@ -1,3 +1,14 @@
+import { Canvas } from "@react-three/fiber";
+
 export default function MySidebar(props) {
-  return <p>Hello from React</p>;
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry />
+        <meshPhongMaterial />
+      </mesh>
+      <ambientLight args={[0xff0000]} intensity={0.1} />
+      <directionalLight position={[0, 0, 5]} intensity={0.5} />
+    </Canvas>
+  );
 }
